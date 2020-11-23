5f9e095def8fb7f316c2d7e307d35b0c7bb6b198
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2020-11-23 20:41:46 +0530

    backend part completed

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+require('dotenv').config(); //having environment variables in .env file
+
+//creating express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json()); //allow us to parse json
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true});
+const connection = mongoose.connection;
+
+connection.once('open',()=>{
+    console.log("MongoDB database connection established successfully");
+});
+
+app.use('/exercises',exercisesRouter);
+app.use('/users',usersRouter);
+
+app.listen(port,()=>{
+    console.log(`Server is running on port : ${port}`);
+});
