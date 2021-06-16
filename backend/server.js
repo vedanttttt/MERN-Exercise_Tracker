a66314112cc7dfb5dd26da84497d3794ccac7c9a
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2021-06-16 20:59:23 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
 
-require('dotenv').config(); //having environment variables in .env file
+require("dotenv").config(); //having environment variables in .env file
 
 //creating express server
 const app = express();
@@ -16,16 +16,25 @@ app.use(cors());
 app.use(express.json()); //allow us to parse json
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true});
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
 
-connection.once('open',()=>{
-    console.log("MongoDB database connection established successfully");
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
-app.use('/exercises',exercisesRouter);
-app.use('/users',usersRouter);
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
-app.listen(port,()=>{
-    console.log(`Server is running on port : ${port}`);
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port : ${port}`);
 });
