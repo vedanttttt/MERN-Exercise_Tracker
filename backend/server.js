d7de22030d300db2db4104fdd3a56720babf8cf3
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2021-06-16 22:39:19 +0530

    back to original

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ connection.once("open", () => {
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
-app.use(express.static(path.join(__dirname, "../build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build"));
-});
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build"));
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`);
